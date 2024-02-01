6e70a7681a869f0eba97c6ee93ea94d9cf59756f
nuburooj <nuburoojkhattak@gmail.com>
2024-01-31 23:17:33 -0500

    complete lab

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+ 
+ counter = 10
+ while counter > 0:
+    print(counter)
+    counter -= 1
+    print("Happy New Year!")
+
+    
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [num ** 2 for num in int_list]
+    
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    num = 1
+    while num <= 100:
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+        num += 1
+
+fizzbuzz()
+   
+    # counter = 100
+    # while counter < 100:
+    #     print(fizzbuzz(num))
+    #     counter += 1
+    #     print(num)
+
+
+
+
